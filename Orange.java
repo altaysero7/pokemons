1f6ce912dd7982f2625b71fe6b1a7963dc75d977
altaysero7 <118746660+altaysero7@users.noreply.github.com>
2022-12-02 00:33:01 +0200

    Update Orange.java

---
+++ Orange.java
@@ -0,0 +1,19 @@
+package Main;
+
+/**
+ *
+ * @author altay
+ */
+public class Orange extends Lutemon {
+
+    public Orange(String name) {
+        this.name = name;
+        color = "Orange";
+        attack = 8;
+        defense = 1;
+        experience = 0;
+        health = 17;
+        maxHealth = 17;
+        id = idCounter;
+    }
+}
