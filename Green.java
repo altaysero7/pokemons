8f0eea221ee00e43e0fd712bdf77958368431172
altaysero7 <118746660+altaysero7@users.noreply.github.com>
2022-12-02 00:31:53 +0200

    Update Green.java

---
+++ Green.java
@@ -0,0 +1,19 @@
+package Main;
+
+/**
+ *
+ * @author altay
+ */
+public class Green extends Lutemon {
+
+    public Green(String name) {
+        this.name = name;
+        color = "Green";
+        attack = 6;
+        defense = 3;
+        experience = 0;
+        health = 19;
+        maxHealth = 19;
+        id = idCounter;
+    }
+}
