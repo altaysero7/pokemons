b069e4b3b402089b33e73ed8adff1b5f325144f3
altaysero7 <118746660+altaysero7@users.noreply.github.com>
2022-12-02 00:33:24 +0200

    Update Pink.java

---
+++ Pink.java
@@ -0,0 +1,19 @@
+package Main;
+
+/**
+ *
+ * @author altay
+ */
+public class Pink extends Lutemon {
+
+    public Pink(String name) {
+        this.name = name;
+        color = "Pink";
+        attack = 7;
+        defense = 2;
+        experience = 0;
+        health = 18;
+        maxHealth = 18;
+        id = idCounter;
+    }
+}
